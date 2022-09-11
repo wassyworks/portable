ec455991fb443ac0736479258b67755f34fa80c5
wassyworks <nasyorz@gmail.com>
2022-09-11 19:34:51 +0900

    - init.lua

---
+++ init.lua
@@ -0,0 +1,13 @@
+
+vim.o.number = true
+vim.o.hlsearch = true
+
+-- カレントディレクトリを自動で移動する
+vim.o.autochdir = true
+
+-- クリップボード
+vim.o.clipboard = "unnamedplus"
+
+-- エンコーディング
+vim.o.fileencoding = "utf-8"
+vim.o.encoding = "utf-8"
